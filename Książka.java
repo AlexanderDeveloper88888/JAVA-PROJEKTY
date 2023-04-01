8103f17a2a5d256b5f936f719a707fb47dc1815f
AlexanderDeveloper88888 <129612506+AlexanderDeveloper88888@users.noreply.github.com>
2023-04-01 18:41:27 +0200

    Add files via upload

---
+++ Książka.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Książka {
+    private String tytuł;
+    private String autor;
+    private int numerSeryjny;
+    private float cena;
+
+    public String getTytuł() {
+        return tytuł;
+    }
+
+    public void setTytuł(String tytuł) {
+        this.tytuł = tytuł;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumerSeryjny() {
+        return numerSeryjny;
+    }
+
+    public void setNumerSeryjny(int numerSeryjny) {
+        this.numerSeryjny = numerSeryjny;
+    }
+
+    public float getCena() {
+        return cena;
+    }
+
+    public void setCena(int cena) {
+        this.cena = cena;
+    }
+
+
+}
+
+
--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+package com.company;
+
+import java.util.ArrayList;
+
 public class Main {
+ArrayList<Książka> książka = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Witaj!");
+        System.out.println("Jeśli chcesz wypożyczyć lub kupić ksiąkę wybierz 1, jeśli chcesz dodać książkę do księgarni wybierz 2");
+        int number = -1;
+        switch (number) {
+        }
+        switch (number) {
+            case 1:
+                System.out.println("Wprowadź ");
+            case 2:
+                System.out.println("");
+            case 3:
+                System.out.println("");
+        }
+        System.out.println("Czy chcesz coś jeszcze zrobić?");
     }
 }
